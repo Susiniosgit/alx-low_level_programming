0a35305fdaaee1284f0a8f89a4acbdc0dc10509a
Susiniosgit <magnawlege@gmail.com>
2021-09-24 03:27:05 -0700

    main.h fixed

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,6 +11,11 @@ char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int *array_range(int min, int max);
 int _putchar(char c);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+
+
 #endif
