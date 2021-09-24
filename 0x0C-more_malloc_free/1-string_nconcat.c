3ce3827c7856a8889980ee5bbd2a62c62e3bd9e6
Susiniosgit <magnawlege@gmail.com>
2021-09-24 03:17:46 -0700

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * string_nconcat - Concatenates two strings using at
- *                  most an inputted number of bytes.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate to s1.
+ * string_nconcat - concatenate two strings
  *
- * Return: If the function fails - NULL.
- *         Otherwise - a pointer to the concatenated space in memory.
+ * @s1: is a pointer to the first string
+ * @s2: is a pointer to the second string
+ * @n: an integer (6) with the limit of what we are going to add to s1
+ *
+ * Return: pointer to a newlly allocated memory with s1 & s2, NULL if it fail
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+	/* unsigned int var because we need to compare with n */
+	char *strings;
+	unsigned int lengths1, lengths2;
 
+	/* task condition if NULL passed, should be treated as empty string */
+	/* same condition of project 0x0B 2-str.concatena */
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	for (index = 0; s1[index]; index++)
-		len++;
-
-	concat = malloc(sizeof(char) * (len + 1));
-
-	if (concat == NULL)
+	/* I go through both strings so I can find their length */
+	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
+	{
+	}
+	for (lengths2 = 0; s2[lengths2] != '\0'; lengths2++)
+	{
+	}
+	/* Now that I know the length of s2 I can compare it with n */
+	/* and set the task condition */
+	if (n > lengths2)
+		n = lengths2;
+	/* now set the malloc with the lenghts */
+	/* n is now equal to lengths2 */
+	/* plus 1 because we need to save space for the last '\0' of strings */
+	strings = malloc((lengths1 + n + 1) * sizeof(char));
+	if (strings == NULL)
 		return (NULL);
-
-	len = 0;
-
-	for (index = 0; s1[index]; index++)
-		concat[len++] = s1[index];
-
-	for (index = 0; s2[index] && index < n; index++)
-		concat[len++] = s2[index];
-
-	concat[len] = '\0';
-
-	return (concat);
+	/* now we add the content of strings to new strings pointer */
+	for (lengths1 = 0; s1[lengths1] != '\0'; lengths1++)
+		strings[lengths1] = s1[lengths1];
+	/* iterate while lengths2 < n so we print only school */
+	/* lenghts1 keeps counting while we go trough s2 */
+	for (lengths2 = 0; lengths2 < n; lengths2++, lengths1++)
+	strings[lengths1] = s2[lengths2];
+	strings[lengths1] = '\0';
+	return (strings);
 }
